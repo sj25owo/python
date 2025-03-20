2c836a4b6843519baf9c8f00a2b310deae5e9d3d
sj25owo <yu03471a@gmail.com>
2025-03-20 22:59:43 +0900

    2주차 예제

---
+++ 예제1.py
@@ -0,0 +1,4 @@
+print("inch 값을 입력 해주세요.")
+string = input("입력 : ")
+inch = float(string)
+print("입력 x 2.89 =", inch * 2.89 , "cm입니다.")
---
+++ 예제2.py
@@ -0,0 +1,4 @@
+print("kg 값을 입력 해주세요.")
+string = input("입력 : ")
+kg = float(string)
+print(" 입력 x 2.205 = ", kg * 2.205 , "ibs입니다.")

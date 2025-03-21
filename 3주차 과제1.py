7b6076487b9d2e60387cca19bc1ecc16a2327016
sj25owo <yu03471a@gmail.com>
2025-03-21 14:22:54 +0900

    과제1

---
+++ 3주차 과제1.py
@@ -0,0 +1,11 @@
+jin = int(input("입력 진수 결정(16/10/8/2) : "))
+gab = input("값 입력 : ")
+sib = int(gab,jin)
+
+print("16진수 ==> "+hex(sib))
+print("10진수 ==> "+str(sib))
+print("8진수 ==> "+oct(sib))
+print("2진수 ==> "+bin(sib))
+
+ 
+   
